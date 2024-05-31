c6637d3bbf69853f82d72e76dcbcc9dc0b7bd6a5
nasrin137 <nasrinakter.hrm@gmail.com>
2024-05-31 20:10:16 +0800

    final commit

---
+++ name.js
@@ -0,0 +1,63 @@
+
+let carName = "Volvo";
+console.log(carName);
+
+// let firstName = "John";
+// let lastName = "Doe";
+// let age = 35;
+// console.log(`Age of ${firstName} ${lastName} is ${age}`);
+
+// var x = 10;
+// var y = 5;
+// x *= y;
+// console.log(x);
+
+
+// let length = 16;   //this is number type data
+// let lastName = "Johnson"; //this is string type data
+// const x = {
+//     firstName:"John",
+//     lastName:"Doe",
+
+// }
+// x is an object and firstName and lastName is string
+
+
+// function myFunction(){
+//     alert("Hello World!")
+// }
+// myFunction()
+
+// const person ={
+//     name:"John",
+//     age:50
+// }
+// alert(person.name + " is " + person.age)
+
+
+// const cars = ["Volvo","Jeep","Mercedes"];
+// alert(cars.length);
+
+// const Brand = ["Volvo","Jeep","Mercedes"];
+// Brand[0] = "Ford";
+// console.log(Brand);
+
+
+// const randomNumber = Math.random();
+// console.log(randomNumber);
+
+// const largestNumber = Math.max(10,20);
+// console.log(largestNumber);
+
+// const squareRoot = Math.sqrt(9);
+// console.log(squareRoot);
+
+
+// const x = 10;
+// const y = 5;
+// alert(x>y);
+
+// const age = 15;
+// const result = age < 18 ? "Too Young" : "Old enough";
+// alert(result);
+
